12c47220b677dbcfb9456a0ba954ba4558d6f097
Amin Ghadersohi <amin.ghadersohi@gmail.com>
2025-10-18 01:26:50 -0400

    fix: update remaining mcpeval references to testmcpy
    - Update GitHub homepage URL in Homebrew formula
    - Update UI package name in package-lock.json

--- Formula/testmcpy.rb
+++ Formula/testmcpy.rb
@@ -2,7 +2,7 @@ class Testmcpy < Formula
   include Language::Python::Virtualenv
 
   desc "MCP Testing Framework - Test LLM tool calling with MCP services"
-  homepage "https://github.com/preset-io/mcpeval"
+  homepage "https://github.com/preset-io/testmcpy"
   url "https://files.pythonhosted.org/packages/source/t/testmcpy/testmcpy-0.1.0.tar.gz"
   sha256 "PLACEHOLDER_WILL_BE_REPLACED_AFTER_PYPI_PUBLISH"
   license "Apache-2.0"
